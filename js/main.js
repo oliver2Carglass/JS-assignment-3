76d19fa3e8149b490b88e8b42e5f9905f2c7e1da
Olivier2Carglass <104084572+oliver2Carglass@users.noreply.github.com>
2023-11-06 00:51:48 -0400

    final commit

--- js/main.js
+++ js/main.js
@@ -1,48 +1,51 @@
-//afficher les elements de Bourne.JSON dans la page HTML avec une boucle for en utilisant fetch
-
-
-
-fetch("Bourne.json")
+fetch("https://raw.githubusercontent.com/oliver2Carglass/Fruits/main/Bourne.json")
     .then(function (response) {
-        return response.json();
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Erreur lors de la récupération du JSON");
+        }
     })
     .then(function (data) {
         console.log(data);
         var outputElement = document.getElementById("output");
-        var htmlContent = "<ul>";
-        for (var key in data) {
-            if (data.hasOwnProperty(key)) {
-                htmlContent += "<li><strong>" + key + ":</strong> " + data[key] + "</li>";
-            }
-        }
-        htmlContent += "</ul>";
-        outputElement.innerHTML = htmlContent;
+
+        data.fruits.forEach(function (fruit) {
+            // Crée un élément div pour chaque fruit
+            var divFruit = document.createElement("div");
+            divFruit.id="fruit"
+
+            // Crée un élément h2 pour le nom du fruit
+            var name = document.createElement("h2");
+            name.textContent = fruit.name;
+            name.id="name"
+
+            // Crée un élément p pour le prix du fruit
+            var price = document.createElement("p");
+            price.textContent = fruit.price;
+            price.id="price"
+
+            // Crée un élément p pour le poids du fruit
+            var weight = document.createElement("p");
+            weight.textContent = "Weight: " + fruit.weight;
+            weight.id="weight"
+
+            // Crée un élément img pour afficher l'image du fruit
+            var image = document.createElement("img");
+            image.src = fruit.image;
+            image.id="Fruit-image"
+
+            // Ajoute les éléments au div du fruit
+            divFruit.appendChild(name);  
+            divFruit.appendChild(image);
+            divFruit.appendChild(weight);
+            divFruit.appendChild(price);
+        
+
+            // Ajoute le div du fruit à la page
+            outputElement.appendChild(divFruit);
+        });
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var outputElement = document.getElementById("output");
-//
-//// Générez du contenu HTML en utilisant les données JSON
-//var htmlContent = "<ul>";
-//for (var key in jsonData) {
-//    if (jsonData.hasOwnProperty(key)) {
-//        htmlContent += "<li><strong>" + key + ":</strong> " + jsonData[key] + "</li>";
-//    }
-//}
-//htmlContent += "</ul>";
-//
-//// Affichez le contenu généré dans la page HTML
-//outputElement.innerHTML = htmlContent;
+    .catch(function (error) {
+        console.error("Erreur lors de la récupération du JSON :", error);
+    });
